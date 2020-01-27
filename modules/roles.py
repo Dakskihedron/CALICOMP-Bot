9fd79f80a6fe95bf28d3c0e26cd13d7c8a3b31b4
Dakski <ghasthunter352@gmail.com>
2020-01-27 22:57:42 +1300

    Added returns and minor changes

--- modules/roles.py
+++ modules/roles.py
@@ -1,10 +1,7 @@
 import discord
-import datetime
 from discord.ext import commands
 
-time_format = "%a, %d %b %Y @ %I:%M:%S %p"
-
-class User(commands.Cog):
+class Roles(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -24,7 +21,7 @@ class User(commands.Cog):
     @rolename.error
     async def rolename_error(self, ctx, error):
         if isinstance(error, commands.CommandInvokeError):
-            await ctx.send(f"{ctx.author.mention} the name must not exceed 100 characters.")
+            return await ctx.send(f"{ctx.author.mention} the name must not exceed 100 characters.")
 
     # Change role colour
     @commands.command(aliases=['rcolour'], brief="Changes your role colour.", description="Changes your role colour.", usage="<colourhexcode>")
@@ -38,46 +35,12 @@ class User(commands.Cog):
             oldcolour = curcolour
             colour = colour.replace('#','')
             await ctx.author.top_role.edit(colour = discord.Colour(int(colour, 16)))
-            await ctx.send(f"{ctx.author.mention} changed your role colour from `{oldcolour}` to `{colour}`.")
-
-    # Display user info
-    @commands.command(aliases=['uinfo'], brief="Displays info about a user.", description="Displays info about a user.\nLeave it blank to display info about yourself.", usage="<optional:user>")
-    @commands.guild_only()
-    async def userinfo(self, ctx, user: discord.Member=None):
-        if not user:
-            embed = discord.Embed(
-                title = f"**{ctx.author}**",
-                colour = ctx.author.colour
-            )
-            embed.set_thumbnail(url=ctx.author.avatar_url)
-            embed.add_field(name="Current display name", value=ctx.author.display_name, inline=False)
-            embed.add_field(name="Joined Discord", value=ctx.author.created_at.strftime(time_format), inline=False)
-            embed.add_field(name="Joined server", value=ctx.author.joined_at.strftime(time_format), inline=False)
-            embed.add_field(name="Highest role in server", value=ctx.author.top_role.mention, inline=False)
-
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(
-                title = f"**{user}**",
-                colour = user.colour
-            )
-            embed.set_thumbnail(url=user.avatar_url)
-            embed.add_field(name="Current display name", value=user.display_name, inline=False)
-            embed.add_field(name="Joined Discord", value=user.created_at.strftime(time_format), inline=False)
-            embed.add_field(name="Joined server", value=user.joined_at.strftime(time_format), inline=False)
-            embed.add_field(name="Highest role in server", value=user.top_role.mention, inline=False)
-
-            await ctx.send(embed=embed)
-
-    @userinfo.error
-    async def userinfo_error(self, ctx, error):
-        if isinstance(error, commands.BadArgument):
-            await ctx.send(f"{ctx.author.mention} please enter a valid user.")
+            await ctx.send(f"{ctx.author.mention} changed your role colour from `{oldcolour}` to `#{colour}`.")
 
     @rolecolour.error
     async def rolecolour_error(self, ctx, error):
         if isinstance(error, commands.CommandInvokeError):
-            await ctx.send(f"{ctx.author.mention} please enter a valid six character colour hex code.")
+            return await ctx.send(f"{ctx.author.mention} please enter a valid six character colour hex code.")
 
 def setup(client):
-    client.add_cog(User(client))
+    client.add_cog(Roles(client))
