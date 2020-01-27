57e026e95025a3bd7bf9cf12b8b2c3d816dac22f
Dakski <ghasthunter352@gmail.com>
2020-01-27 15:46:45 +1300

    Added coinflip command and minor changes

--- modules/fun.py
+++ modules/fun.py
@@ -6,7 +6,39 @@ class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    # Magic eight ball command
+    # Coinflip
+    @commands.command(aliases=['coin', 'cointoss'], brief="Flip a coin.", description="Flip a coin.")
+    @commands.guild_only()
+    async def coinflip(self, ctx):
+        coin = ["heads", "tails"]
+        random.shuffle(coin)
+        side = random.choice(coin)
+        await ctx.send(f"{ctx.author.mention} {side}.")
+
+    # Decision
+    @commands.command(aliases=['choose', 'pick'], brief="Randomly chooses from inputed choices.", description="Randomly chooses from inputed choices.", usage="<choice1|choice2|...>")
+    @commands.guild_only()
+    async def decide(self, ctx, *, choices : str):
+        alist = choices.split("|")
+        random.shuffle(alist)
+        answer = random.choice(alist)
+        await ctx.send(f"{ctx.author.mention} {answer}")
+
+    # Dice 
+    @commands.command(aliases=['die', 'rng'], brief="Random number generator.", description="Random number generator. Enter a number larger than zero or one.")
+    @commands.guild_only()
+    async def dice(self, ctx, number : float):
+        if (number).is_integer():
+            await ctx.send(f"{ctx.author.mention} {random.randrange(1, number)}")
+        else:
+            return await ctx.send(f"{ctx.author.mention} please enter a whole number.")
+
+    @dice.error
+    async def dice_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send(f"{ctx.author.mention} please enter a number larger than zero or one.")
+
+    # Magic eight ball
     @commands.command(aliases=['8ball'], brief="Seek advice or fortune-telling.", description="Seek advice or fortune-telling. Provide a question.")
     @commands.guild_only()
     async def eightball(self, ctx, question):
@@ -39,30 +71,7 @@ class Fun(commands.Cog):
     @eightball.error
     async def eightball_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"{ctx.author.mention} you did not provide a question.")
-
-    # Dice command
-    @commands.command(aliases=['die', 'rng'], brief="Random number generator.", description="Random number generator. Enter a number larger than zero or one.")
-    @commands.guild_only()
-    async def dice(self, ctx, number : float):
-        if (number).is_integer():
-            await ctx.send(f"{ctx.author.mention} {random.randrange(1, number)}")
-        else:
-            return await ctx.send(f"{ctx.author.mention} please enter a whole number.")
-
-    @dice.error
-    async def dice_error(self, ctx, error):
-        if isinstance(error, commands.CommandInvokeError):
-            await ctx.send(f"{ctx.author.mention} please enter a number larger than zero or one.")
-
-    # Decision command
-    @commands.command(aliases=['choose', 'choice', 'pick', 'wheel'], brief="Randomly chooses from inputed choices.", description="Randomly chooses from inputed choices. Separate choices with vertical bar.")
-    @commands.guild_only()
-    async def decide(self, ctx, *, choices : str):
-        alist = choices.split("|")
-        random.shuffle(alist)
-        answer = random.choice(alist)
-        await ctx.send(f"{ctx.author.mention} {answer}")
+            await ctx.send(f"{ctx.author.mention} please enter a question.")
 
 def setup(client):
     client.add_cog(Fun(client))
