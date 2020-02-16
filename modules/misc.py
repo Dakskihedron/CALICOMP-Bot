c2be8d16332a7c37b853925e958b4a4347548a53
Dakski <ghasthunter352@gmail.com>
2020-02-16 23:35:03 +1300

    Imported new handler for time formatting

--- modules/misc.py
+++ modules/misc.py
@@ -1,8 +1,7 @@
 import discord
-import datetime
-from discord.ext import commands
 
-time_format = "%a, %d %b %Y @ %I:%M:%S %p"
+from discord.ext import commands
+from utils import default
 
 class Misc(commands.Cog):
     def __init__(self, client):
@@ -18,30 +17,18 @@ class Misc(commands.Cog):
     @commands.command(aliases=['uinfo', 'whois'], brief="Displays info about a user.", description="Displays info about a user.\nLeave it blank to display info about yourself.", usage="<user>")
     @commands.guild_only()
     async def userinfo(self, ctx, user: discord.Member=None):
-        if not user:
-            embed = discord.Embed(
-                title = f"**{ctx.author}**",
-                colour = ctx.author.colour
-            )
-            embed.set_thumbnail(url=ctx.author.avatar_url)
-            embed.add_field(name="Current display name", value=ctx.author.display_name, inline=False)
-            embed.add_field(name="Joined Discord", value=ctx.author.created_at.strftime(time_format), inline=False)
-            embed.add_field(name="Joined server", value=ctx.author.joined_at.strftime(time_format), inline=False)
-            embed.add_field(name="Highest role in server", value=ctx.author.top_role.mention, inline=False)
+        user = user or ctx.author
+        embed = discord.Embed(
+            title = f"**{user}**",
+            colour = user.colour
+        )
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.add_field(name="Current display name", value=user.display_name, inline=False)
+        embed.add_field(name="Joined Discord", value=default.date(user.created_at), inline=False)
+        embed.add_field(name="Joined server", value=default.date(user.joined_at), inline=False)
+        embed.add_field(name="Highest role in server", value=user.top_role.mention, inline=False)
 
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(
-                title = f"**{user}**",
-                colour = user.colour
-            )
-            embed.set_thumbnail(url=user.avatar_url)
-            embed.add_field(name="Current display name", value=user.display_name, inline=False)
-            embed.add_field(name="Joined Discord", value=user.created_at.strftime(time_format), inline=False)
-            embed.add_field(name="Joined server", value=user.joined_at.strftime(time_format), inline=False)
-            embed.add_field(name="Highest role in server", value=user.top_role.mention, inline=False)
-
-            await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
     @userinfo.error
     async def userinfo_error(self, ctx, error):
